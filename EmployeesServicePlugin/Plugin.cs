e7b0210cf97fafc0dc2a4773e0d702c4f7abb93c
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-06-26 23:31:36 +0300

    Инициализирован EmployeesServicePlugin

---
+++ EmployeesServicePlugin/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesServicePlugin
+{
+    [Author(Name = "Ivan Shatalov")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info(AppDomain.CurrentDomain.BaseDirectory + "___EmployeeServicePlugin");
+            return new List<DataTransferObject>(args);
+        }
+    }
+}
